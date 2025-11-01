ec7c892ad8a4256f10686db88672dd7b2583abd2
sanket-hasher <sanketdeyng11c@gmail.com>
2025-11-01 10:42:52 +0530

    first commit

---
+++ src/main/java/com/empexchng/empexchng/EmpexchngApplication.java
@@ -0,0 +1,13 @@
+package com.empexchng.empexchng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpexchngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpexchngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empexchng/empexchng/EmpexchngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empexchng.empexchng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpexchngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
